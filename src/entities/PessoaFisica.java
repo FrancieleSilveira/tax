e747fb8f93408030b6e24aea47dc004cba9a8daf
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-24 22:45:53 -0300

    finalizado

--- src/entities/PessoaFisica.java
+++ src/entities/PessoaFisica.java
@@ -2,4 +2,32 @@ package entities;
 
 public class PessoaFisica extends Pessoa{
 
+	private Double gastosSaude;
+	
+	public PessoaFisica() {
+		super();
+	}
+
+	public PessoaFisica(String nome, Double rendaAnual, Double gastosSaude) {
+		super(nome, rendaAnual);
+		this.gastosSaude = gastosSaude;
+	}
+
+	public Double getGastosSaude() {
+		return gastosSaude;
+	}
+
+	public void setGastosSaude(Double gastosSaude) {
+		this.gastosSaude = gastosSaude;
+	}
+
+	@Override
+	public double imposto() {
+		if (super.getRendaAnual() < 20000) {
+			return 0.15 * super.getRendaAnual() - (0.5 * gastosSaude);
+		}
+		return 0.25 * super.getRendaAnual() - (0.5 * gastosSaude);
+	}
+	
+	
 }
--- src/entities/PessoaJuridica.java
+++ src/entities/PessoaJuridica.java
@@ -3,4 +3,31 @@ package entities;
 public class PessoaJuridica extends Pessoa{
 
 	private Integer numFuncionarios;
+	
+	public PessoaJuridica() {
+		super();
+	}
+
+	public PessoaJuridica(String nome, Double rendaAnual, Integer numFuncionarios) {
+		super(nome, rendaAnual);
+		this.numFuncionarios = numFuncionarios;
+	}
+
+	public Integer getNumFuncionarios() {
+		return numFuncionarios;
+	}
+
+	public void setNumFuncionarios(Integer numFuncionarios) {
+		this.numFuncionarios = numFuncionarios;
+	}
+
+	@Override
+	public double imposto() {
+		if (numFuncionarios > 10) {
+			return super.getRendaAnual() * 0.14;
+		}
+		return super.getRendaAnual() * 0.16;
+	}
+	
+	
 }
